06adcdacaf0ba562c428d4112af87c32fb195fea
frankfang <frankfang1990@gmail.com>
2018-01-13 18:03:48 +0800

    promise

--- main.js
+++ main.js
@@ -6,47 +6,56 @@ window.jQuery = function(nodeOrSelector){
 }
 window.$ = window.jQuery
 
-window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
-  let request = new XMLHttpRequest()
-  request.open(method, url) // 配置request
-  for(let key in headers) {
-    let value = headers[key]
-    request.setRequestHeader(key, value)
+window.Promise = function(fn){
+  // ...
+  return {
+    then: function(){}
   }
-  request.onreadystatechange = ()=>{
-    if(request.readyState === 4){
-      if(request.status >= 200 && request.status < 300){
-        successFn.call(undefined, request.responseText)
-      }else if(request.status >= 400){
-        failFn.call(undefined, request)
-      }
-    }
-  }
-  request.send(body)
 }
 
-function f1(responseText){}
-function f2(responseText){}
+window.jQuery.ajax = function({url, method, body, headers}){
+  return new Promise(function(resolve, reject){
+    let request = new XMLHttpRequest()
+    request.open(method, url) // 配置request
+    for(let key in headers) {
+      let value = headers[key]
+      request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = ()=>{
+      if(request.readyState === 4){
+        if(request.status >= 200 && request.status < 300){
+          resolve.call(undefined, request.responseText)
+        }else if(request.status >= 400){
+          reject.call(undefined, request)
+        }
+      }
+    }
+    request.send(body)
+  })
+}
 
 myButton.addEventListener('click', (e)=>{
-  window.jQuery.ajax({
-    url: '/frank',
+  let promise = window.jQuery.ajax({
+    url: '/xxx',
     method: 'get',
     headers: {
       'content-type':'application/x-www-form-urlencoded',
       'frank': '18'
-    },
-    successFn: (x)=>{
-      f1.call(undefined,x)
-      f2.call(undefined,x)
-    },
-    failFn: (x)=>{
-      console.log(x)
-      console.log(x.status)
-      console.log(x.responseText)
     }
   })
+
+  promise.then(
+    (text)=>{console.log(text)},
+    (request)=>{console.log(request)}
+  )
+
 })
+
+
+return new Promise(function(resolve, reject)){
+
+}
+
 
 
 
